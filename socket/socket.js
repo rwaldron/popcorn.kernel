dd64e461cf35dc1dc6e4e61746f8e28ebcf04949
Rick Waldron <waldron.rick@gmail.com>
2011-09-11 19:37:33 -0400

    Socket stubs in full working order

--- socket/socket.js
+++ socket/socket.js
@@ -1,10 +1,10 @@
 var app = require( "express" ).createServer(),
 		io = require( "socket.io" ).listen(app);
 
-app.listen( 80 );
+app.listen( 8080 );
 
 app.get( "/", function( req, res ) {
-	res.sendfile( __dirname + "/test/socket.html" );
+	res.sendfile( __dirname + "/index.html" );
 });
 
 io.sockets.on( "connection", function( socket ) {
